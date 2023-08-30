162ff2ee47ebdf24dc18227d0769a0660379888b
hoanglong5 <longch.dev@gmail.com>
2023-08-30 14:13:36 +0700

    Init project

---
+++ src/main/java/com/hoanglong/backendecommercerestapi/BackendecommercerestapiApplication.java
@@ -0,0 +1,13 @@
+package com.hoanglong.backendecommercerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendecommercerestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendecommercerestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoanglong/backendecommercerestapi/BackendecommercerestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoanglong.backendecommercerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendecommercerestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
